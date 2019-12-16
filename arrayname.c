0a9c971ae7b25f12f4f9bd8c606f5c43352667a2
Jegadeesh Vontlin <2364552+JegadeeshVontlin@users.noreply.github.com>
2019-12-16 08:40:29 +0530

    Update arrayname.c

--- arrayname.c
+++ arrayname.c
@@ -1,16 +1,60 @@
 #include <stdio.h>
 
+
+char *name;
+int *age,choice,i;
+
+
+
+void add(char *add_name, int *add_age) 
+{
+	
+	char value_name[10];
+	int  value_age[10];
+	add_name = value_name;
+	add_age = value_age;
+	
+	// Getting the input upto 10 names
+	for(i=0;i<10;i++) {
+		scanf("%s", add_name);	
+		add_name++;	
+	}
+	
+	// Getting the input upto 10 age
+	for (i=0;i<10;i++) {
+		scanf("%d", add_age);
+		add_age++;
+	}
+	
+}
+
+/*void delete(*name)*/
+/*{*/
+/*	// Delete the array from the given name*/
+/*}*/
+
+/*void display() */
+/*{*/
+/*	// free all the record */
+/*}*/
+
+/*void array_exit()*/
+/*{*/
+/* return 0;*/
+/*}*/
+
 void main()
 {
-  char *name;
-  int age,choice;
-  scanf("%s %d", name, age);
 
-  printf(" 1.Add 2.Delete 3.Display all record 4.Exit the program \n Enter your choice:");
-  scanf("%d, &choice");
+/*  printf("Enter Your name and age: \n");*/
+/*  scanf("%s %d",name,&age);*/
+  
+
+  printf("1.Add \t\t\t2.Delete \n3.Display all record \t4.Exit the program \nEnter your choice:");
+  scanf("%d",&choice);
 
   if(choice == 1) {
-    add(name,age);
+    add(*name,*age);
   }
   else if(choice == 2) {
     if( (name) && (age) !=NULL) ) {
@@ -30,7 +74,7 @@ void main()
     printf("All records deleted");
   }
   else(choice == 4) {
-    return 0;
+	array_exit();
   }
  
-}
+}  
