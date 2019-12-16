6bdf5302247ac88773b6922f1c9408ccbe956917
Jegadeesh Vontlin <2364552+JegadeeshVontlin@users.noreply.github.com>
2019-12-16 10:24:08 +0530

    Update arrayname.c

--- arrayname.c
+++ arrayname.c
@@ -1,80 +1,66 @@
 #include <stdio.h>
 
 
-char *name;
-int *age,choice,i;
-
+char *name, value_name[10];
+int *age,choice,i,count=0,value_age[10];
+name = value_name;
+age = value_age;
 
 
 void add(char *add_name, int *add_age) 
-{
-	
-	char value_name[10];
-	int  value_age[10];
-	add_name = value_name;
-	add_age = value_age;
-	
-	// Getting the input upto 10 names
+{	
+	// Getting the input upto 10 names and age
 	for(i=0;i<10;i++) {
 		scanf("%s", add_name);	
-		add_name++;	
-	}
-	
-	// Getting the input upto 10 age
-	for (i=0;i<10;i++) {
 		scanf("%d", add_age);
+		add_name++;
 		add_age++;
+		count++;
 	}
 	
 }
 
-void delete(*name)
+void delete(char *del_name)
 {
 	// Delete the array from the given name
-}
-
-void display() 
-{
-	// free all the record 
-}
-
-void array_exit()
-{
- return 0;
+	for(i=0;i<count;i++) {
+		name[i]= "";		
+	}
 }
 
 void main()
 {
 
-/*  printf("Enter Your name and age: \n");*/
-/*  scanf("%s %d",name,&age);*/
-  
-
   printf("1.Add \t\t\t2.Delete \n3.Display all record \t4.Exit the program \nEnter your choice:");
   scanf("%d",&choice);
 
   if(choice == 1) {
-    add(*name,*age);
+  	if(count <=10) {
+	    add(*name,*age);
+    } else {
+    	printf("Maximum 10 records can be added");
+    }
   }
   else if(choice == 2) {
-    if( (name) && (age) !=NULL) ) {
-      printf("Enter the name you would like to delete");
+  	  printf("Enter the name you would like to delete");
+  	  name:
       scanf("%d", &name);
-      if(*name==NULL) {
-        printf("Enter a name ");
+      if(name!=NULL) {
+      	delete(name);
+      }
+      else {
+      	printf("Please enter a valid name");
+      	goto name;
       }
     }
   }
-  else if (*name == match_found() ) {
-    printf("Record Found: %s",name);
-  }
-  else if( choice == 3) {
-    free(name);
-    free(age);
-    printf("All records deleted");
+  else if (choice == 3) {
+    for(i=0;i<count;i++) {
+    	printf("%s", name[i])
+		printf("\t %d \n", age[i])
+    }
   }
   else(choice == 4) {
-	array_exit();
-  }
- 
+	exit();  
+  } 
 }  
