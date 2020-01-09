f351013080b24c183b44eebb21926d19e381fc9f
Jegadeesh Vontlin <2364552+JegadeeshVontlin@users.noreply.github.com>
2020-01-09 17:22:57 +0530

    Update main.c
    Working on last input

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#include "calculator.h"
+#include "calculator.h"
 
-void main() {
-	int i,sum=0,count=0;
-	char ascii='0', *ptr, operation[24];
+int main() {
+	int i,count=0,sum=0, ddsum=0;
+	char *ptr,operation[24],dd_operation[1],op, dp;
 	ptr = operation;
+	char error[] = "Calculator Doesn't support more than 10 digits";
 	for(i=0;i<23;i++)
  	{
 		ptr[i] = getc(stdin);	
@@ -15,42 +16,42 @@ void main() {
 			break;		
 		}
 	}
-			
-/*	while(ptr[i] = getc(stdin)	) {*/
-/*			i++;*/
-/*			count++;*/
-/*		if(ptr[i] == 0x0A) {*/
-/*			break;*/
-/*		}*/
-/*	}*/
 
 	if((ptr[0] == 'a') && (ptr[1] == 'd') && (ptr[2] == 'd'))  {
-		//addition(operation, count, );
-	for(i=3;i<count;i++) {
+		//addition(ptr, count, op);	
+		for(i=3;i<count;i++) {
 			if (ptr[i] == ' ')  {
 				// Do nothing
+				i++;
+				sum += atoi((ptr+i));
 			}	
-			i++;
-			//printf("pointer[%d] is %d \n", i, atoi((ptr+i)));		
-			sum += atoi((ptr+i));
-		}	
-		putc(sum,stdout);
+			else {								
+				if( (ptr[i] == ' ' ) && ( count == 21 ) ) {				
+					//dp  = ptr[++i] + ptr[++i];
+					//ddsum = atoi((char*)dp);
+					sum += dp + atoi((ptr+i)); 
+				}
+				else{
+					sum += atoi((ptr+i));
+				}
+			}
+		}
+		printf("%d\n", sum);
 	}
-	
-/*	else if(ptr_oprn[0] == 's' && ptr_oprn[1] == 'u' && ptr_oprn[2] == 'b') {*/
-/*		subraction(*ptr_oprn);*/
+/*	else if((ptr[0] == 's') && (ptr[1] == 'u') && (ptr[2] == 'b')) {*/
+/*		subtraction(ptr, count, op);	*/
 /*	}*/
 /*	*/
-/*	else if(ptr_oprn[0] == 'm' && ptr_oprn[1] == 'u' && ptr_oprn[2] == 'l') {*/
-/*		multiplication(*ptr_oprn);*/
+/*	else if(ptr[0] == 'm' && ptr[1] == 'u' && ptr[2] == 'l') {*/
+/*		multiplication(ptr, count, op);*/
 /*	}*/
 /*		*/
-/*	else if(ptr_oprn[0] == 'd' && ptr_oprn[1] == 'i' && ptr_oprn[2] == 'v') {*/
-/*		division(*ptr_oprn);*/
+/*	else if(ptr[0] == 'd' && ptr[1] == 'i' && ptr[2] == 'v') {*/
+/*		division(ptr, count, op);*/
 /*	}*/
 /*		*/
-/*	else if(ptr_oprn[0] == 'm' && ptr_oprn[1] == 'o' && ptr_oprn[2] == 'd') {*/
-/*		modulo(*ptr_oprn);*/
+/*	else if(ptr[0] == 'm' && ptr[1] == 'o' && ptr[2] == 'd') {*/
+/*		modulo(ptr, count, op);*/
 /*	}*/
 /*	else {*/
 /*		// the operation not supported should be printed out using putc , think the logic*/
